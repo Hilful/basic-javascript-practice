36ea1840b1163998b3865f42f4a6ea0ae8e2cb4b
Hilful <hilful25@gmail.com>
2022-02-02 02:22:38 +0600

    2nd commit

---
+++ dogAgetoHumanAge.js
@@ -0,0 +1,8 @@
+var dogAge = prompt('How old is your Dog ?');
+
+
+var dogAgeInInt = parseInt(dogAge);
+
+var humanAge = (dogAgeInInt - 2) * 4 + 21
+
+alert('Age of the Human should be ' + humanAge);
