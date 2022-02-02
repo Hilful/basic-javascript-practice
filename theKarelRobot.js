4ddc726381efae2906922ffbe805041135b39d10
Hilful <hilful25@gmail.com>
2022-02-02 23:19:33 +0600

    added theKarelRobot solution

---
+++ theKarelRobot.js
@@ -0,0 +1,20 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+ function main(){
+    moveInDirection();
+    turnLeft();
+    moveInDirection();
+ }
+ 
+ 
+ function moveInDirection(){
+ move();
+ move();
+ move();
+ move();
+ 
+ 
+ }
